13a340a77eadd82a729b8e528779ff842196d936
hinakauserkokan <hinakokan20@gmail.com>
2020-04-21 20:55:22 +0530

    url validation done

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 function shortIt(bigUrl){
-    alert(bigUrl)
+    var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+        if (pattern.test(bigUrl)) {
+            alert("Url is valid");
+            return true;
+        } 
+            alert("Url is not valid!");
+            return false;
 }
